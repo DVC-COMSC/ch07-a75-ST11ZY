c6f4e9299f89c350330af5335afe868ce5f75621
Your Name <you@example.com>
2022-10-11 12:52:03 -0700

    real

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import sys
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-if len(num1) > len(num2):
-	print ('False')
-	sys.exit(0)
+for i in range(len(num2)-len(num1)+1):
+	if num1==num2[i:len(num1)]:
+		print("True")
+		break
+else:
+	print("False")
 # ******************************
 # Make your Code
 # ******************************
