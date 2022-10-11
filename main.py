7d3446066ea10e8bd4d669db6d3aee971af92b4b
Your Name <you@example.com>
2022-10-11 12:53:21 -0700

    real

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ for i in range(len(num2)-len(num1)+1):
 	if num1==num2[i:len(num1)]:
 		print("True")
 		break
-else:
-	print("False")
+	else:
+		print("False")
 # ******************************
 # Make your Code
 # ******************************
